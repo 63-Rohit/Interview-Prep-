5b345aa961ae3a67d14558733b3480f9b5ba9723
rohit_639 <rohit391174@gmail.com>
2026-08-08 10:45:18 +0530

    Remove executable files

---
+++ Arrays/linear_search.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int arr[100];
+    cout<<"Enter the size of array: ";
+    cin>>n;
+
+    cout<<"Enter the elements in an array : ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+}
